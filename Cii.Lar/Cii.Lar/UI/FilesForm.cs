43215e1b0f42c9d3c7be0b4d4037424541ab7383
Vincent <zhongw_1988@126.com>
2017-07-13 22:09:01 +0800

    获取文件夹文件显示在imagelistview上

--- Cii.Lar/Cii.Lar/UI/FilesForm.cs
+++ Cii.Lar/Cii.Lar/UI/FilesForm.cs
@@ -1,9 +1,13 @@
+using Cii.Lar.SysClass;
 using DevComponents.DotNetBar;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
@@ -14,6 +18,22 @@ namespace Cii.Lar
         public FilesForm()
         {
             InitializeComponent();
+            string folderName = SysConfig.GetSysConfig().StorePath;
+            string[] extesnsions = new string[] { ".png", ".wmv" };
+            var files = GetFiles(folderName, extesnsions, SearchOption.TopDirectoryOnly);
+            foreach (var file in files)
+            {
+                imageListView.Items.Add(file.ToString());
+            }
+        }
+
+        private IEnumerable GetFiles(string sourceDirectory, string[] exts, SearchOption searchOpt)
+        {
+            return Directory.GetFiles(sourceDirectory, "*.*", searchOpt)
+                    .Where(
+                inS => exts.Contains(System.IO.Path.GetExtension(inS),
+                StringComparer.OrdinalIgnoreCase)
+                           );
         }
     }
 }
