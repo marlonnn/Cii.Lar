2b22d8eb6a97e3c9bc9f36239a2308cd69927c42
marlonnn <zhongw_1988@126.com>
2017-07-27 14:30:38 +0800

    解决绘制矩形时只能绘制第四象限的问题

--- Cii.Lar/Cii.Lar/DrawTools/DrawRectangle.cs
+++ Cii.Lar/Cii.Lar/DrawTools/DrawRectangle.cs
@@ -16,6 +16,11 @@ namespace Cii.Lar.DrawTools
     /// </summary>
     public class DrawRectangle : DrawObject
     {
+        protected float dataLeft;
+        protected float dataRight;
+        protected float dataTop;
+        protected float dataBottom;
+
         private Rectangle rectangle;
         public DrawRectangle()
         {
@@ -36,10 +41,44 @@ namespace Cii.Lar.DrawTools
         {
             g.SmoothingMode = SmoothingMode.AntiAlias;
 
+            Rectangle r = GetNormalizedRectangle(GetRectangle());
             using (Pen pen = new Pen(this.Color, PenWidth))
             {
-                g.DrawRectangle(pen, rectangle);
+                rectangle.Offset(MovingOffset);
+                g.DrawRectangle(pen, r);
             }
+        }
+
+        public static Rectangle GetNormalizedRectangle(Rectangle r)
+        {
+            return GetNormalizedRectangle(r.X, r.Y, r.X + r.Width, r.Y + r.Height);
+        }
+
+        /// <summary>
+        /// Get normalized rectangle when move left or top
+        /// </summary>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="x2"></param>
+        /// <param name="y2"></param>
+        /// <returns></returns>
+        public static Rectangle GetNormalizedRectangle(int x1, int y1, int x2, int y2)
+        {
+            if (x2 < x1)
+            {
+                int tmp = x2;
+                x2 = x1;
+                x1 = tmp;
+            }
+
+            if (y2 < y1)
+            {
+                int tmp = y2;
+                y2 = y1;
+                y1 = tmp;
+            }
+
+            return new Rectangle(x1, y1, x2 - x1, y2 - y1);
         }
 
         /// <summary>
@@ -86,6 +125,30 @@ namespace Cii.Lar.DrawTools
                     break;
             }
             rectangle = new Rectangle(left, top, right - left, bottom - top);
+            SetRectangle(rectangle);
+        }
+
+        private void SetRectangle(Rectangle r)
+        {
+            PointF dataLeftTop =  new PointF(r.Left, r.Top);
+            PointF dataRightBottom = new PointF(r.Right, r.Bottom);
+            dataLeft = dataLeftTop.X;
+            dataTop = dataLeftTop.Y;
+            dataRight = dataRightBottom.X;
+            dataBottom = dataRightBottom.Y;
+        }
+
+        protected Rectangle GetRectangle()
+        {
+            return ToDrawRectangle(dataLeft, dataRight, dataTop, dataBottom);
+        }
+
+        private Rectangle ToDrawRectangle(float left, float right, float top, float bottom)
+        {
+            Point leftTop = Point.Ceiling(new PointF(left, top));
+            Point rightBottom = Point.Ceiling(new PointF(right, bottom));
+
+            return new Rectangle(leftTop.X, leftTop.Y, rightBottom.X - leftTop.X, rightBottom.Y - leftTop.Y);
         }
     }
 }
