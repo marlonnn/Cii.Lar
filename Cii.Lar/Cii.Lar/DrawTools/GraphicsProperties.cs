0ce779f2e7dd59ed8826ce106e1dacb7b1ab2b3f
marlonnn <zhongw_1988@126.com>
2017-07-27 16:48:57 +0800

    添加GraphicsProperties图形属性类

---
+++ Cii.Lar/Cii.Lar/DrawTools/GraphicsProperties.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cii.Lar.DrawTools
+{
+    /// <summary>
+    /// Graphics property
+    /// Author:Zhong Wen 2017/07/25
+    /// </summary>
+    public class GraphicsProperties
+    {
+        private Color? color;
+        public Color? Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                color = value;
+            }
+        }
+        private int? penWidth;
+        public int? PenWidth
+        {
+            get
+            {
+                return penWidth;
+            }
+            set
+            {
+                penWidth = value;
+            }
+        }
+
+        public GraphicsProperties()
+        {
+            color = System.Drawing.Color.Black;
+            penWidth = 1;
+        }
+    }
+}
