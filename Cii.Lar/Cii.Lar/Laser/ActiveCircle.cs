ebcff29f3721ee00a80469251bb266e9517a7a18
marlonnn <zhongw_1988@126.com>
2017-10-20 13:55:33 +0800

    添加ActiveCircle活检激光类；

---
+++ Cii.Lar/Cii.Lar/Laser/ActiveCircle.cs
@@ -0,0 +1,155 @@
+﻿using Cii.Lar.DrawTools;
+using Cii.Lar.UI;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cii.Lar.Laser
+{
+    public class ActiveCircle
+    {
+        private Point startPoint;
+        public Point StartPoint
+        {
+            get { return startPoint; }
+            set
+            {
+                if (value != Point.Empty)
+                {
+                    if (clickCount % 2 == 1)
+                    {
+                        startPoint = value;
+                        StartCircle = new Circle(value, InnerCircleSize);
+                    }
+                }
+            }
+        }
+
+        private Point endPoint;
+        public Point EndPoint
+        {
+            get { return this.endPoint; }
+            set
+            {
+                if (value != Point.Empty && clickCount % 2 != 0)
+                {
+                    endPoint = value;
+                    EndCircle = new Circle(value, InnerCircleSize);
+                }
+            }
+        }
+
+        private Circle startCircle;
+
+        public Circle StartCircle
+        {
+            get { return this.startCircle; }
+            set { this.startCircle = value; }
+        }
+
+        private Circle endCircle;
+
+        public Circle EndCircle
+        {
+            get { return this.endCircle; }
+            set { this.endCircle = value; }
+        }
+
+        public Size InnerCircleSize { get; set; }
+
+        private ZWPictureBox pictureBox;
+
+        private int clickCount;
+
+        public ActiveCircle(ZWPictureBox pictureBox)
+        {
+            this.pictureBox = pictureBox;
+            InnerCircleSize = new Size(38, 38);
+            clickCount = 0;
+        }
+
+        public void OnMouseDown(Point point)
+        {
+            clickCount++;
+            StartPoint = point;
+            EndPoint = point;
+        }
+
+        public void OnMouseMove(Point point, int dx, int dy)
+        {
+            EndPoint = point;
+            CalculateContinuousCircle(dx, dy);
+        }
+
+        private void CalculateContinuousCircle(int dx, int dy)
+        {
+            var length = Math.Sqrt(dx * dx + dy * dy);
+            int num = (int)(length / InnerCircleSize.Width);
+            if (dx == 0)
+            {
+                if (length > num * this.InnerCircleSize.Width && length <= (num + 1) * this.InnerCircleSize.Width)
+                {
+                    if (dy > 0)
+                    {
+                        var count = (int)(2 * length / InnerCircleSize.Width);
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+            else if (dy == 0)
+            {
+
+            }
+            else
+            {
+                var k = dy / dx;
+            }
+        }
+
+        public void OnPaint(Graphics g)
+        {
+            if (startCircle.CenterPoint.IsEmpty || endCircle.CenterPoint.IsEmpty)
+            {
+                return;
+            }
+
+            DrawCross(g);
+        }
+
+        private void DrawCross(Graphics g)
+        {
+            using (Pen circlePen = new Pen(Color.Yellow, 2f))
+            using (Pen pen = new Pen(Color.Black, 1f))
+            {
+                //draw start circle
+                g.DrawEllipse(circlePen, startCircle.Rectangle);
+
+                //draw start point cross
+                g.DrawLine(pen, startCircle.CenterPoint.X, startCircle.CenterPoint.Y - startCircle.Rectangle.Width / 2,
+                startCircle.CenterPoint.X, startCircle.CenterPoint.Y + startCircle.Rectangle.Width / 2);
+                g.DrawLine(pen, startCircle.CenterPoint.X - startCircle.Rectangle.Width / 2, startCircle.CenterPoint.Y,
+                    startCircle.CenterPoint.X + startCircle.Rectangle.Width / 2, startCircle.CenterPoint.Y);
+                //draw end point cross
+                g.DrawLine(pen, EndCircle.CenterPoint.X, EndCircle.CenterPoint.Y - EndCircle.Rectangle.Width / 2,
+                    EndCircle.CenterPoint.X, EndCircle.CenterPoint.Y + EndCircle.Rectangle.Width / 2);
+                g.DrawLine(pen, EndCircle.CenterPoint.X - EndCircle.Rectangle.Width / 2, EndCircle.CenterPoint.Y,
+                    EndCircle.CenterPoint.X + EndCircle.Rectangle.Width / 2, EndCircle.CenterPoint.Y);
+
+                //draw connect Line
+                g.DrawLine(pen, startCircle.CenterPoint, endCircle.CenterPoint);
+
+                //draw end circle
+                g.DrawEllipse(circlePen, EndCircle.Rectangle);
+            }
+        }
+
+
+    }
+}
--- Cii.Lar/Cii.Lar/Laser/ActiveLaser.cs
+++ Cii.Lar/Cii.Lar/Laser/ActiveLaser.cs
@@ -16,164 +16,38 @@ namespace Cii.Lar.Laser
         private Point mouseDownPoint;
         private Point endPoint;
 
-        private Circle outterCircle = null;
-        public Circle OutterCircle
-        {
-            get { return outterCircle; }
-            private set
-            {
-                if (value != outterCircle)
-                {
-                    outterCircle = value;
-                }
-            }
-        }
-
-        private Circle innerCircle = null;
-        public Circle InnerCircle
-        {
-            get { return innerCircle; }
-            private set
-            {
-                if (value != innerCircle)
-                {
-                    innerCircle = value;
-                }
-            }
-        }
-
-        public PointF CenterPoint
-        {
-            get;
-            set;
-        }
-
-        public Size OutterCircleSize { get; set; }
-
-        public Size InnerCircleSize { get; set; }
-
-        private List<Circle> outterCircles = null;
-        public List<Circle> OutterCircles
-        {
-            get { return outterCircles; }
-            set
-            {
-                if (value != outterCircles)
-                {
-                    outterCircles = value;
-                }
-            }
-        }
-
-        private List<Circle> innerCircles = null;
-        public List<Circle> InnerCircles
-        {
-            get { return innerCircles; }
-            set
-            {
-                if (value != innerCircles)
-                {
-                    innerCircles = value;
-                }
-            }
-        }
-
+        private ActiveCircle activeCircle;
         public ActiveLaser(ZWPictureBox pictureBox) : base()
         {
             this.pictureBox = pictureBox;
-            OutterCircleSize = new Size(60, 60);
-            InnerCircleSize = new Size(38, 38);
-
-            InnerCircles = new List<Circle>();
-            OutterCircles = new List<Circle>();
-            this.GraphicsProperties.GraphicsPropertiesChangedHandler += GraphicsPropertiesChangedHandler;
-        }
-        private void GraphicsPropertiesChangedHandler(DrawObject drawObject, GraphicsProperties graphicsProperties)
-        {
-            OutterCircleSize = new Size((60 + this.GraphicsProperties.ExclusionSize) * this.GraphicsProperties.TargetSize,
-                (60 + this.GraphicsProperties.ExclusionSize) * this.GraphicsProperties.TargetSize);
-            InnerCircleSize = new Size(38 * this.GraphicsProperties.TargetSize, 38 * this.GraphicsProperties.TargetSize);
-            OutterCircle = new Circle(CenterPoint, OutterCircleSize);
-            InnerCircle = new Circle(CenterPoint, InnerCircleSize);
-            this.pictureBox.Invalidate();
+            activeCircle = new ActiveCircle(pictureBox);
         }
 
         public override void OnMouseDown(ZWPictureBox pictureBox, MouseEventArgs e)
         {
-
             mouseDownPoint = e.Location;
 
             Point point = new Point((int)(e.X / pictureBox.Zoom - pictureBox.OffsetX), 
                 (int)(e.Y / pictureBox.Zoom - pictureBox.OffsetY));
-            CenterPoint = new PointF(point.X, point.Y);
 
-            OutterCircle = new Circle(CenterPoint, OutterCircleSize);
-            InnerCircle = new Circle(CenterPoint, InnerCircleSize);
-
-            if (InnerCircles.Count == 0)
-            {
-                InnerCircles.Add(InnerCircle);
-            }
-            if (OutterCircles.Count == 0)
-            {
-                OutterCircles.Add(OutterCircle);
-            }
-
-            if (InnerCircles.Count == 1)
-            {
-                InnerCircles[0].CenterPoint = CenterPoint;
-            }
-            if (OutterCircles.Count == 1)
-            {
-                OutterCircles[0].CenterPoint = CenterPoint;
-            }
+            activeCircle.OnMouseDown(point);
             this.pictureBox.Invalidate();
         }
 
         public override void OnMouseMove(ZWPictureBox pictureBox, MouseEventArgs e)
         {
-            //if (e.Button == MouseButtons.Left)
-            {
-                Point point = new Point((int)(e.X / pictureBox.Zoom - pictureBox.OffsetX),
-                    (int)(e.Y / pictureBox.Zoom - pictureBox.OffsetY));
+            Point point = new Point((int)(e.X / pictureBox.Zoom - pictureBox.OffsetX),
+                (int)(e.Y / pictureBox.Zoom - pictureBox.OffsetY));
 
-                Point mousePosNow = e.Location;
+            Point mousePosNow = e.Location;
 
-                endPoint = point;
+            endPoint = point;
 
-                int dx = mousePosNow.X - mouseDownPoint.X;
-                int dy = mousePosNow.Y - mouseDownPoint.Y;
-                var length = Math.Sqrt(dx * dx + dy * dy);
-                //Console.WriteLine("delt length : " + length);
-
-                int num = (int)(length / this.InnerCircleSize.Width);
-                //Console.WriteLine("num : " + num);
-
-                if (dx == 0)
-                {
-
-                }
-                //if (length > num * this.InnerCircleSize.Width && length <= (num + 1) * this.InnerCircleSize.Width)
-                //{
-                //    if (InnerCircles.Count == num + 1)
-                //    {
-                //        InnerCircles.Add(new Circle(new PointF(point.X, point.Y), InnerCircleSize));
-                //    }
-                //    if (InnerCircles.Count == num + 2)
-                //    {
-                //        InnerCircles[num + 1].CenterPoint = new PointF(point.X, point.Y);
-                //    }
-                //    if (OutterCircles.Count == num + 1)
-                //    {
-                //        OutterCircles.Add(new Circle(new PointF(point.X, point.Y), OutterCircleSize));
-                //    }
-                //    if (OutterCircles.Count == num + 2)
-                //    {
-                //        OutterCircles[num + 1].CenterPoint = new PointF(point.X, point.Y);
-                //    }
-                //}
-                this.pictureBox.Invalidate();
-            }
+            int dx = mousePosNow.X - mouseDownPoint.X;
+            int dy = mousePosNow.Y - mouseDownPoint.Y;
+            var length = Math.Sqrt(dx * dx + dy * dy);
+            activeCircle.OnMouseMove(point, dx, dy);
+            this.pictureBox.Invalidate();
         }
 
         public override void OnMouseUp(ZWPictureBox pictureBox, MouseEventArgs e)
@@ -187,54 +61,9 @@ namespace Cii.Lar.Laser
 
             g.CompositingQuality = CompositingQuality.HighQuality;
             g.SmoothingMode = SmoothingMode.AntiAlias;
-            SolidBrush brush = new SolidBrush(this.GraphicsProperties.Color);
 
-            GraphicsPath path1 = new GraphicsPath();
-            GraphicsPath path2 = new GraphicsPath();
-            Region region1 = new Region();
-            Region region2 = new Region();
-            for (int i = 0; i < this.OutterCircles.Count; i++)
-            {
-                path1.AddEllipse(OutterCircles[i].Rectangle.X, OutterCircles[i].Rectangle.Y,
-                    OutterCircles[i].Rectangle.Width, OutterCircles[i].Rectangle.Height);
-
-                path2.AddEllipse(InnerCircles[i].Rectangle.X, InnerCircles[i].Rectangle.Y,
-                    InnerCircles[i].Rectangle.Width, InnerCircles[i].Rectangle.Height);
-                if (i == 0)
-                {
-                    region1 = new Region(path1);
-                    region2 = new Region(path2);
-                }
-                region1.Union(new Region(path1));
-                region2.Union(new Region(path2));
-            }
-            region1.Exclude(region2);
-            g.FillRegion(brush, region1);
-            DrawLine(g);
-            brush.Dispose();
-            path1.Dispose();
-            path2.Dispose();
-            region1.Dispose();
+            activeCircle.OnPaint(g);
         }
 
-        private void DrawCross(Graphics g)
-        {
-            g.DrawLine(new Pen(Color.Black, 1f),
-                InnerCircle.CenterPoint.X, InnerCircle.CenterPoint.Y - InnerCircle.Rectangle.Width / 2,
-                InnerCircle.CenterPoint.X, InnerCircle.CenterPoint.Y + InnerCircle.Rectangle.Width / 2);
-
-            g.DrawLine(new Pen(Color.Black, 1f),
-                InnerCircle.CenterPoint.X - InnerCircle.Rectangle.Width / 2, InnerCircle.CenterPoint.Y,
-                InnerCircle.CenterPoint.X + InnerCircle.Rectangle.Width / 2, InnerCircle.CenterPoint.Y);
-        }
-
-        private void DrawLine(Graphics g)
-        {
-            if (CenterPoint.IsEmpty || endPoint.IsEmpty)
-            {
-                return;
-            }
-            g.DrawLine(new Pen(Color.Black, 1f), CenterPoint, endPoint);
-        }
     }
 }
